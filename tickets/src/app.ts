6386a2806f86055568442927deec88a067e5e34b
Mattias Mucherie <mattias.mattias.m@gmail.com>
2021-08-27 16:45:15 +0200

    [WIP] ticketing service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,10 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { NotFoundError, errorHandler } from "@mm-tickets/common";
+import { NotFoundError, errorHandler, currentUser } from "@mm-tickets/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -13,6 +16,10 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
