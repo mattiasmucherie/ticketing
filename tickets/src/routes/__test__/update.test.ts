bf164f773b83e1a5b5cb113ecb78ce5315b4b8bf
Mattias Mucherie <mattias.mattias.m@gmail.com>
2021-08-31 10:06:41 +0200

    Ticketing service completed

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -8,7 +8,7 @@ it("returns a 404 if the provided id does not exist", async () => {
   await request(app)
     .put(`/api/tickets/${id}`)
     .set("Cookie", global.signin())
-    .send({ title: "ljkhoiuyfdsas", price: 20 })
+    .send({ title: "something", price: 20 })
     .expect(404);
 });
 
@@ -17,12 +17,64 @@ it("returns a 401 if the user is not authenticated", async () => {
 
   await request(app)
     .put(`/api/tickets/${id}`)
-    .send({ title: "ljkhoiuyfdsas", price: 20 })
+    .send({ title: "something", price: 20 })
     .expect(401);
 });
 
-it("returns a 401 if the user does not own the ticket", async () => {});
+it("returns a 401 if the user does not own the ticket", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "something",
+      price: 20,
+    });
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", global.signin())
+    .send({ title: "something2", price: 1000 })
+    .expect(401);
+});
 
-it("returns a 400 if the user provides an invalid title or price", async () => {});
+it("returns a 400 if the user provides an invalid title or price", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "something",
+      price: 20,
+    });
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "", price: 1000 })
+    .expect(400);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "something", price: -20 })
+    .expect(400);
+});
 
-it("updates teh ticket provided valid inputs", async () => {});
+it("updates teh ticket provided valid inputs", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "something",
+      price: 20,
+    });
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "new title", price: 1000 })
+    .expect(200);
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual("new title");
+  expect(ticketResponse.body.price).toEqual(1000);
+});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -33,7 +33,10 @@ afterAll(async () => {
 
 global.signin = () => {
   // Build a JWT payload. {id, email}
-  const payload = { email: "test@test.com", id: "123456789" };
+  const payload = {
+    email: "test@test.com",
+    id: new mongoose.Types.ObjectId().toHexString(),
+  };
   // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
   // Build session Object {jwt: MY_JWT}
@@ -42,6 +45,6 @@ global.signin = () => {
   const sessionJSON = JSON.stringify(session);
   // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString("base64");
-  // return a string thats the cookie with the encoded data
+  // return a string that's the cookie with the encoded data
   return [`express:sess=${base64}`];
 };
